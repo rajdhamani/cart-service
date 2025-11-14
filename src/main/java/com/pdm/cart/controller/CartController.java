721a6ba61a6f8c25935cec2a4ac9ea8b5fefa084
rajdhamani <tech.rajsharma@gmail.com>
2025-11-14 17:18:07 +0530

    initial commit

---
+++ src/main/java/com/pdm/cart/controller/CartController.java
@@ -0,0 +1,15 @@
+package com.pdm.cart.controller;
+
+import com.pdm.cart.model.Order;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cart-service")
+public class CartController {
+
+    public Order addToCart(){
+
+        return null;
+    }
+}
---
+++ src/main/java/com/pdm/cart/model/Order.java
@@ -0,0 +1,4 @@
+package com.pdm.cart.model;
+
+public class Order {
+}
